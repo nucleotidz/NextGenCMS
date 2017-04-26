806abdffc80273bf4be077ae311fd46dea711409
Shobhit Tiwari <shbht_twr@yahoo.com>
2017-04-26 19:18:04 +0530

    task list count

--- NextGenCMS.UI/Scripts/controller/HeaderController.js
+++ NextGenCMS.UI/Scripts/controller/HeaderController.js
@@ -1,9 +1,14 @@
 ﻿(function () {
     'use strict';
-    app.controller('HeaderController', ['$scope', 'Cache',
-    function ($scope, Cache) {
+    app.controller('HeaderController', ['$scope', 'Cache','WorkFlowAPI','$q',
+    function ($scope, Cache, WorkFlowAPI, $q) {
         var vm = this;
         var displayName = Cache.get('displayName');
         vm.Name = displayName;
+
+        var tasks = WorkFlowAPI.GetWorkFlow();
+        $q.all([tasks.$promise]).then(function (response) {
+            vm.taskCount = response.length;
+        });
     }]);
 })();
